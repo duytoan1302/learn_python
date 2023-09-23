2cfc978c190b08f34cd8b930fc8c6c27dea99eb9
duytoan1302 <145834934+duytoan1302@users.noreply.github.com>
2023-09-23 15:18:33 +0700

    upload my first lesson in Python
    Create my character (daddy helped me to do this)

---
+++ create_character.py
@@ -0,0 +1,13 @@
+print("Please make your character!")
+name=input("What your character name?")
+age=input("How old your character?")
+strengths=input("What are your character power?")
+weaknesses=input("What are your character weaknesses?")
+print("Your character's name is", name)
+print("Your character is", age, "years old")
+print("Strengths are:", strengths)
+print("Weaknesses are:", weaknesses)
+
+# dong nay giong nhu dong tiep theo
+#print(name, " says \"thank you for making me!\"")
+print(name, 'says "thank you for making me!"')
