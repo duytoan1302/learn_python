961fc2eff82f7f59d86195b9a57bd047a83fe430
duytoan1302 <145834934+duytoan1302@users.noreply.github.com>
2023-09-30 11:16:03 +0700

    Create robot_if_else.py

---
+++ robot_if_else.py
@@ -0,0 +1,11 @@
+user_reply = input("Do you like robot? (please fill in yes or no or maybe or just like the eyes lazer)")
+if user_reply == "yes":
+       print("bip bip")
+elif user_reply "maybe":
+       print("dicisively dress people")
+elif user_reply == "just like the eyes lazer":
+       print("reeet")
+elif user_reply == "no":
+       print("Robot is don't like you so much")
+else:
+       print("we don't understand what to say, they don't like us")
